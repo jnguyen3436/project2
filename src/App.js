d1efb48f52831b8c0dea72141d57f95fa4069143
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-19 11:41:44 -0500

    added button and functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state ={
+    advice :[]
+  }
+
+  componentDidMount(){
+    axios.get("https://api.adviceslip.com/advice").then(advice=>{
+      this.setState({advice:advice.data.slip})
+      
+      console.log(advice.data.slip)
+    
+      
+    })
+    
+  }
+
+  getAdvice=()=>{
+    axios.get("https://api.adviceslip.com/advice").then(advice=>{
+      this.setState({advice:advice.data.slip})
+      
+      console.log(advice.data.slip)
+    
+      
+    })
+  }
+   
+
+  render() {
+    return (
+      <div>
+        {this.state.advice.advice}
+        <button onClick={this.getAdvice}>Click Here Grace!!</button>
+      </div>
+    )
+  }
 }
+  
 
-export default App;
+ 
