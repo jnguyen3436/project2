b634695e0d388e27423abd9613a423f087d024f7
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-21 10:53:44 -0500

    get the list

--- src/List.js
+++ src/List.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class List extends Component {
+    state = {theList:[]}
 
-    show
+    getTheList =()=>{
+        axios.get("https://ih-beers-api2.herokuapp.com/beers",).then(profile=>{
+          console.log(profile)
+          this.setState({theList:profile})
+        })
+    }
+    showList=()=>{
+
+    }
     render() {
         return (
             <div>
